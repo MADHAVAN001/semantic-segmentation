b4e2b680fcabc740e5501376e02104b5f233bf24
Madhavan Seshadri <MADHAVAN001@e.ntu.edu.sg>
2019-11-30 04:12:18 -0500

    Sparsification changes

---
+++ models/fcn_sparsify.py
@@ -0,0 +1,202 @@
+from tensorflow.keras.backend import image_data_format
+from tensorflow.keras.models import Model
+from tensorflow.keras.regularizers import l2
+from tensorflow_model_optimization.sparsity import keras as sparsity
+
+from models.BilinearUpSampling import *
+
+
+class FCN:
+
+    def __init__(self, sparsify=False, sparsify_params=None):
+        self.is_pruning_enabled = sparsify
+        if self.is_pruning_enabled:
+            self.prune_params = {'pruning_schedule':
+                                     sparsity.PolynomialDecay(initial_sparsity=sparsify_params[0],
+                                                              final_sparsity=sparsify_params[1],
+                                                              begin_step=sparsify_params[2],
+                                                              end_step=sparsify_params[3],
+                                                              frequency=sparsify_params[4])}
+
+    def identity_block(self, kernel_size, filters, stage, block, weight_decay=0., momentum=0.99):
+        """The identity block is the block that has no conv layer at shortcut.
+        # Arguments
+            input_tensor: input tensor
+            kernel_size: default 3, the kernel size of
+                middle conv layer at main path
+            filters: list of integers, the filters of 3 conv layer at main path
+        """
+
+        def f(input_tensor):
+            filters1, filters2, filters3 = filters
+            if image_data_format() == 'channels_last':
+                bn_axis = 3
+            else:
+                bn_axis = 1
+
+            convolution_base_name = 'res' + str(stage) + block + '_branch'
+            bn_base_name = 'bn' + str(stage) + block + '_branch'
+
+            x = Conv2D(
+                filters1,
+                (1, 1),
+                use_bias=False,
+                kernel_initializer='he_normal',
+                kernel_regularizer=l2(weight_decay)
+            )(input_tensor)
+
+            if self.is_pruning_enabled:
+                x = sparsity.prune_low_magnitude(x, **self.prune_params)
+
+            x = BatchNormalization(axis=bn_axis, momentum=momentum)(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(
+                filters2,
+                kernel_size,
+                padding='same',
+                use_bias=False,
+                kernel_initializer='he_normal',
+                kernel_regularizer=l2(weight_decay)
+            )(x)
+            if self.is_pruning_enabled:
+                x = sparsity.prune_low_magnitude(x, **self.prune_params)
+
+            x = BatchNormalization(
+                axis=bn_axis,
+                momentum=momentum
+            )(x)
+
+            x = Activation('relu')(x)
+
+            x = Conv2D(
+                filters3,
+                (1, 1),
+                use_bias=False,
+                kernel_initializer='he_normal',
+                kernel_regularizer=l2(weight_decay)
+            )(x)
+            if self.is_pruning_enabled:
+                x = sparsity.prune_low_magnitude(x, **self.prune_params)
+
+            x = BatchNormalization(axis=bn_axis, momentum=momentum)(x)
+
+            x = add([x, input_tensor])
+            x = Activation('relu')(x)
+            return x
+
+        return f
+
+    def convolution_block(self, kernel_size, filters, stage, block, strides=(2, 2), weight_decay=0., momentum=0.99,
+                          epsilon=0.):
+        """ Definition of Convolutional Block in a typical Resnet
+        # Arguments
+            input_tensor: input tensor
+            kernel_size: default 3, the kernel size of
+                middle conv layer at main path
+            filters: list of integers, the filters of 3 conv layer at main path
+            stage: integer, current stage label, used for generating layer names
+        # Returns
+            Output tensor for the block.
+        Note that from stage 3,
+        the second conv layer at main path is with strides=(2, 2)
+        And the shortcut should have strides=(2, 2) as well
+        """
+
+        def f(input_tensor):
+            filters1, filters2, filters3 = filters
+
+            if image_data_format() == 'channels_last':
+                bn_axis = 3
+            else:
+                bn_axis = 1
+
+            convolution_base_name = 'res' + str(stage) + block + '_branch'
+            bn_base_name = 'bn' + str(stage) + block + '_branch'
+
+            x = Conv2D(filters1, (1, 1), use_bias=False, kernel_initializer='he_normal',
+                       kernel_regularizer=l2(weight_decay))(input_tensor)
+
+            if self.is_pruning_enabled:
+                x = sparsity.prune_low_magnitude(x, **self.prune_params)
+
+            x = BatchNormalization(axis=bn_axis, momentum=momentum)(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(filters2, kernel_size, strides=strides, padding='same', use_bias=False,
+                       kernel_initializer='he_normal', kernel_regularizer=l2(weight_decay))(x)
+
+            if self.is_pruning_enabled:
+                x = sparsity.prune_low_magnitude(x, **self.prune_params)
+
+            x = BatchNormalization(axis=bn_axis, momentum=momentum)(x)
+            x = Activation('relu')(x)
+
+            x = Conv2D(filters3, (1, 1), use_bias=False, kernel_initializer='he_normal',
+                       kernel_regularizer=l2(weight_decay))(x)
+
+            if self.is_pruning_enabled:
+                x = sparsity.prune_low_magnitude(x, **self.prune_params)
+
+            x = BatchNormalization(axis=bn_axis, momentum=momentum)(x)
+
+            shortcut = Conv2D(filters3, (1, 1), strides=strides, use_bias=False, kernel_initializer='he_normal',
+                              kernel_regularizer=l2(weight_decay))(input_tensor)
+            shortcut = BatchNormalization(axis=bn_axis, momentum=momentum)(shortcut)
+
+            x = add([x, shortcut])
+            x = Activation('relu')(x)
+            return x
+
+        return f
+
+    def resnet50(self, input_shape=None, weight_decay=0., momentum=0.9, batch_shape=None, classes=21):
+        if batch_shape:
+            img_input = Input(batch_shape=batch_shape)
+            image_size = batch_shape[1:3]
+        else:
+            img_input = Input(shape=input_shape)
+            image_size = input_shape[0:2]
+
+        bn_axis = 3
+
+        x = Conv2D(64, (7, 7), strides=(2, 2), padding='same', name='conv1', kernel_regularizer=l2(weight_decay))(
+            img_input)
+        x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+        x = Activation('relu')(x)
+        x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+        x = self.convolution_block(3, [64, 64, 256], stage=2, block='a', strides=(1, 1))(x)
+        x = self.identity_block(3, [64, 64, 256], stage=2, block='b')(x)
+        x = self.identity_block(3, [64, 64, 256], stage=2, block='c')(x)
+
+        x = self.convolution_block(3, [128, 128, 512], stage=3, block='a')(x)
+        x = self.identity_block(3, [128, 128, 512], stage=3, block='b')(x)
+        x = self.identity_block(3, [128, 128, 512], stage=3, block='c')(x)
+        x = self.identity_block(3, [128, 128, 512], stage=3, block='d')(x)
+
+        x = self.convolution_block(3, [256, 256, 1024], stage=4, block='a')(x)
+        x = self.identity_block(3, [256, 256, 1024], stage=4, block='b')(x)
+        x = self.identity_block(3, [256, 256, 1024], stage=4, block='c')(x)
+        x = self.identity_block(3, [256, 256, 1024], stage=4, block='d')(x)
+        x = self.identity_block(3, [256, 256, 1024], stage=4, block='e')(x)
+        x = self.identity_block(3, [256, 256, 1024], stage=4, block='f')(x)
+
+        x = self.convolution_block(3, [512, 512, 2048], stage=5, block='a')(x)
+        x = self.identity_block(3, [512, 512, 2048], stage=5, block='b')(x)
+        x = self.identity_block(3, [512, 512, 2048], stage=5, block='c')(x)
+
+        x = Conv2D(classes,
+                   (1, 1),
+                   kernel_initializer='he_normal',
+                   activation='linear',
+                   padding='valid',
+                   strides=(1, 1),
+                   kernel_regularizer=l2(weight_decay))(x)
+
+        x = BilinearUpSampling2D(size=(32, 32))(x)
+
+        outputs = Conv2D(classes, (1, 1), activation='sigmoid')(x)
+        model = Model(inputs=[img_input], outputs=[outputs])
+
+        return model
--- utils/performance.py
+++ utils/performance.py
@@ -1,11 +1,9 @@
-from tensorflow.keras.callbacks import Callback
-import os
-import six
 import io
-import tensorflow.keras.backend as K
-import numpy as np
-import tensorflow as tf
 import time
+
+import numpy as np
+import tensorflow.keras.backend as K
+from tensorflow.keras.callbacks import Callback
 
 
 class PerformanceMetrics(Callback):
@@ -15,47 +13,33 @@ class PerformanceMetrics(Callback):
         self.filename = filename
         self.append = append
         self.start_train_time = None
-        if six.PY2:
-            self.file_flags = 'b'
-            self._open_args = {}
-        else:
-            self.file_flags = ''
-            self._open_args = {'newline': '\n'}
+        self.csv_file = None
 
     def on_train_begin(self, logs=None):
-        if self.append:
-            if os.path.exists(self.filename):
-                with open(self.filename, 'r' + self.file_flags) as f:
-                    self.append_header = not bool(len(f.readline()))
-            mode = 'a'
-        else:
-            mode = 'w'
-        self.csv_file = io.open(self.filename,
-                                mode + self.file_flags,
-                                **self._open_args)
+        self.csv_file = io.open(self.filename, 'w')
+
+    def headers(self):
+        row = list()
+        row.append("epoch")
+        row.append("trainable_parameters")
+        row.append("non_trainable_parameters")
+        row.append("total_parameters")
+        row.append("epoch_training_time")
+
+        self.csv_file.write(",".join(row))
 
     def on_epoch_begin(self, epoch, logs=None):
         self.start_train_time = time.time()
 
     def on_epoch_end(self, epoch, logs=None):
-        trainable_count = int(
-            np.sum([K.count_params(p) for p in set(self.model.trainable_weights)]))
-        non_trainable_count = int(
-            np.sum([K.count_params(p) for p in set(self.model.non_trainable_weights)]))
+        trainable_count = np.sum([K.count_params(w) for w in self.model.trainable_weights])
+        non_trainable_count = np.sum([K.count_params(w) for w in self.model.non_trainable_weights])
 
         row = list()
+        row.append(str(epoch))
         row.append(str(trainable_count))
         row.append(str(non_trainable_count))
-        row.append(str(self.estimate_flops()))
+        row.append(str(trainable_count + non_trainable_count))
         row.append(str(time.time() - self.start_train_time))
 
         self.csv_file.write(",".join(row))
-
-    def estimate_flops(self):
-        run_meta = tf.RunMetadata()
-        opts = tf.profiler.ProfileOptionBuilder.float_operation()
-
-        flops = tf.profiler.profile(graph=K.get_session().graph,
-                                    run_meta=run_meta, cmd='op', options=opts)
-
-        return flops.total_float_ops
